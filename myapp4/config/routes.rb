3523fd4ae96b211d15774831048b771c548109da
kousuke-c <sentoerumo0558@gmail.com>
2021-10-13 09:02:05 +0000

    add feature

--- myapp4/config/routes.rb
+++ myapp4/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get'/',to:'schedules#index'
+  get'new',to:'schedules#new'
+  post'new',to:'schedules#create'
+  post'update',to:'schedules#update'
+  
+   resources:schedules
 end
